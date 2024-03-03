c4bff02c84f7f6ebbbc0a6038e0f527934a79666
aadityaadyot <aadityaadyot@gmail.com>
2024-03-04 01:17:24 +0530

    evaluation file updated and dvc pipeline updated

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_training import TrainingPipeline
+from cnnClassifier.pipeline.stage_05_evaluation import EvaluationPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -34,3 +36,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e    
+
+
+STAGE_NAME = "Evaluation"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
